a7c83218cbb38d0e22ce73ce2c5e5f5de3638acb
Nagata-999 <y.nagata@jissengakuen.ed.jp>
2026-07-15 11:43:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sushitan-v1";
+const CACHE_NAME = "sushitan-v2";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -8,9 +8,7 @@ const APP_SHELL = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
@@ -25,7 +23,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
   event.respondWith(
     fetch(event.request)
       .then(response => {
