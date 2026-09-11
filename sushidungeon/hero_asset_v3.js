df5b10e84493aaf99956f8736c0ff1909a62b025
Nagata-999 <y.nagata@jissengakuen.ed.jp>
2026-09-12 07:49:36 +0900

    Use directional rendered hero asset as primary visual

--- sushidungeon/hero_asset_v3.js
+++ sushidungeon/hero_asset_v3.js
@@ -1,7 +1,7 @@
 'use strict';
 (function(){
   const s=document.createElement('script');
-  s.src='https://raw.githubusercontent.com/Nagata-999/Ewords/adf4cc0be9ca55fad9b554f4494c5ed65ec4ad83/sushidungeon/hero_asset_v3.js';
+  s.src='https://raw.githubusercontent.com/Nagata-999/Ewords/aeef4502b4e74e06f103ea54217499f3b3ecc412/sushidungeon/hero_asset_v3.js';
   s.async=false;
   document.head.appendChild(s);
 })();
