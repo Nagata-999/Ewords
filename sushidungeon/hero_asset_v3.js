0046cba9affdb5150b0500a155dbbcbeaa76be89
Nagata-999 <y.nagata@jissengakuen.ed.jp>
2026-09-12 06:41:08 +0900

    Restore stable hero renderer after directional asset experiment

--- sushidungeon/hero_asset_v3.js
+++ sushidungeon/hero_asset_v3.js
@@ -1,4 +1,7 @@
 'use strict';
 (function(){
-const HERO={
-s:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAYAAADDPmHLAAAoS0lEQVR42u2deXwUV77vP+eEJIQQQgghhBBCiDBRZFXBBVnxIKuKoqi...
+  const s=document.createElement('script');
+  s.src='https://raw.githubusercontent.com/Nagata-999/Ewords/adf4cc0be9ca55fad9b554f4494c5ed65ec4ad83/sushidungeon/hero_asset_v3.js';
+  s.async=false;
+  document.head.appendChild(s);
+})();
